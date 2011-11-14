67ea8bdd834cb5ea4b6f45f3726f9bc72cd6cf1d
Debasish Ghosh <debasish@debasishg.(none)>
2011-11-14 16:48:07 +0530

    minor refactoring

--- src/main/scala/net/debasishg/recon/Util.scala
+++ src/main/scala/net/debasishg/recon/Util.scala
@@ -3,11 +3,7 @@ package net.debasishg.recon
 import scalaz._
 import Scalaz._
 
-import org.scala_tools.time.Imports._
-
 object Util {
-  def as[T, U](me: T)(implicit f: T => U) = f(me)
-
   def zipMap[A, B, C](l1: List[A], l2: List[B])(f: (A, B) => C) =
     l1 zip l2 map Function.tupled(f)
 
